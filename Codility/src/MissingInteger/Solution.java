5c543ae6f753fa67dc218ecf0876668de8a3ac4b
Piotr Basinski <piotrbasinski@Piotrs-MacBook-Pro.local>
2018-03-20 13:35:19 +0100

    add missingIngeger lesson

---
+++ Codility/src/MissingInteger/Solution.java
@@ -0,0 +1,20 @@
+package MissingInteger;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+class Solution {
+    public static int solution(int[] A) {
+
+        Integer[] array = Arrays.stream(A).boxed().toArray(Integer[]::new);
+        HashSet<Integer> set = new HashSet(Arrays.asList(array));
+
+        int minValue = 1;
+
+        while (set.contains(minValue)) {
+            minValue++;
+        }
+
+        return minValue;
+    }
+}
